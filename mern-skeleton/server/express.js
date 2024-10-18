41bbe2d1e656ec937e975af5f37e314d0fc89be7
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-10-18 23:32:51 +0100

    configuring Express

---
+++ mern-skeleton/server/express.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const compress = require('compression')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
+module.exports = app
