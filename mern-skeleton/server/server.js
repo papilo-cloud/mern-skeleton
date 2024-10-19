5fde938cf530b8a5b90fdfcc9d4deb3407c80bac
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-10-19 01:41:17 +0100

    implementing the server

--- mern-skeleton/server/server.js
+++ mern-skeleton/server/server.js
@@ -1,9 +1,28 @@
 const config = require('../config/config')
 const app = require('./express')
+const mongoose = require('mongoose')
 
+mongoose.set('strictQuery', false)
+
+main().catch(err => console.error(err))
+async function main() {
+    const client = await mongoose.connect(config.mongoUri, 
+    {
+        dbName: 'blog'
+    },
+    {
+        useFindAndModify: true,
+        useCreateIndex: true
+    })
+}
+
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to DB: ${config.mongoUri}`);
+    
+})
 app.listen(config.port, (err) => {
     if (err) {
         console.error(err)
     }
-    console.log('Server started on port %s.', config.port)
+    console.log('Server started on port %s',config.port)
 })
