cabac8ab5303df3b9be31e50b2020ff5c88f68bd
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-10-19 01:42:53 +0100

    UI template

---
+++ mern-skeleton/template.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+    return `<!doctype html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <title>Mern Skeleton</title>
+            </head>
+            <body>
+                <div id="root">Hello World</div>
+            </body>
+        </html>
+    `
+}
