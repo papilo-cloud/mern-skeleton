974631b7cd678dff03762f595cb0e25f65fa3889
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-10-23 22:59:41 +0100

    auth controller

---
+++ mern-skeleton/server/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+const User = require("../models/user.model");
+const jwt = require('jsonwebtoken')
+const exp_jwt = require('express-jwt');
+const config = require("../../config/config");
+
+
+exports.signin = (req, res) => {
+
+}
+
+exports.signout = (req, res) => {
+
+}
+
+exports.requireSignin = (req, res) => {}
+exports.hasAuthorization = (req, res) => {}
