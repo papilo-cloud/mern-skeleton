00b99be6e0b323bfd999e2eca64830092c4d2279
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-11-26 14:19:55 +0100

    Add update, delete, and get user routes logic

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,27 +1,9 @@
-const express = require('express')
+const bcrypt = require('bcrypt')
 const User = require('../model/users.model')
 const getErrorMessage = require('../lib/dbErrorHandlers')
-const encryptPassword = require('../lib/encrypt_password')
 
 
-let createUser = async (req, res) => {
-    try {
-        const password = await encryptPassword(req.body.password)
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password
-        })
-        const users = await user.save()
-        res.status(201).json(users)
-    } catch (err) {
-        return res.status(400).json({
-            error: getErrorMessage(err)
-        })
-    }
-}
-
-let getUsers = async (req, res) => {
+let getUsersRoute = async (req, res) => {
     try {
         let users = await User.find()
         res.json(users)
@@ -32,8 +14,49 @@ let getUsers = async (req, res) => {
     }
 }
 
+let userPolicy = async (req) => {
+    let user = await User.findById(req.params.userId)
+    let profile = req.profile
+    return user.id === profile.id
+}
+
+let updateUserRoute = async (req, res) => {
+    if (req.body.password) {
+        const salt = await bcrypt.genSalt(10)
+        req.body.password = await bcrypt.hash(req.body.password, salt)
+    }
+    try {
+        const updateUser = await User.findByIdAndUpdate(req.params.userId, req.body, {new: true})
+        res.status(200).json(updateUser)
+    } catch (err) {
+        res.status(401).json({error: 'You can only delete your account!'})
+    }
+}
+
+let deleteUserRoute = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId)
+        try {
+            await User.findByIdAndDelete(req.params.userId)
+            res.status(200).json({
+                error: 'User has been deleted...'
+            })
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    } catch (err) {
+        res.status(500).json('User Not Found')
+    }
+}
+
+let getUserRoute = (req, res) => {
+    return res.json(req.profile)
+}
 
 module.exports = {
-    createUser,
-    getUsers
+    getUsersRoute,
+    updateUserRoute,
+    deleteUserRoute,
+    getUserRoute,
+    userPolicy
 }
