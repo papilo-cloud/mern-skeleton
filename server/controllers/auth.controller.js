56130e5faf1b8c79315ae15cfe8ed41678d27891
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-11-26 14:16:52 +0100

    Add signout logic

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,6 +2,27 @@ const jwt = require("jsonwebtoken")
 const authenticate = require("../lib/authenticate_password")
 const User = require("../model/users.model")
 const config = require("../config/config")
+const getErrorMessage = require('../lib/dbErrorHandlers')
+const encryptPassword = require('../lib/encrypt_password')
+
+
+let createUserRoute = async (req, res) => {
+    try {
+        const password = await encryptPassword(req.body.password)
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password,
+            subject: req.body.subject
+        })
+        const users = await user.save()
+        res.status(201).json(users)
+    } catch (err) {
+        return res.status(400).json({
+            error: getErrorMessage(err)
+        })
+    }
+}
 
 let signin = async (req, res) => {
     try {
@@ -18,26 +39,34 @@ let signin = async (req, res) => {
         }
 
         const token = jwt.sign(
-            {id: user.id},
+            {userId: user.id},
             config.jwtSecret
         )
-        res.cookies('t', token, {expire: new Date() + 9999})
+        console.log(token)
+        res.cookie('t', token, {expire: new Date() + 9999})
 
         return res.json({
             token,
             user: {
                 id: user.id,
                 name: user.name,
-                email: user.email
+                email: user.email,
+                subject: user.subject
             }
         })
         
     } catch (err) {
         res.status(401).json({
-            error: 'Could not sign in.'
+            error: 'Could not sign in'
         })
     }
 }
 
+let signout = (req, res) => {
+    res.clearCookie('t')
+    return res.status(200).json({
+        message: 'signed out'
+    })
+}
 
-module.exports = {signin}
+module.exports = {signin, createUserRoute, signout}
