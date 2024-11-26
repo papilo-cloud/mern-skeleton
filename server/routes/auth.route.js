b27293cc46e36c2442e7ae233f794435fe82c596
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-11-26 14:25:41 +0100

    Add signout route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,12 @@
-const { signin } = require('../controllers/auth.controller')
+const { signin, createUserRoute, signout } = require('../controllers/auth.controller')
+const validate = require('../lib/validate_password')
 
 const authRoute = require('express').Router()
 
 authRoute.route('/signin').post(signin)
-// authRoute.route('/').get(signin)
+authRoute.route('/signout').get(signout)
+authRoute.route('/register')
+    .post(validate, createUserRoute)
 
 
 module.exports = authRoute
