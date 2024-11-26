f71640d5d78c9796ed97a28ec853ff6b2599e351
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-11-26 14:28:10 +0100

    Add tokenAuth

--- server/lib/token-auth.js
+++ server/lib/token-auth.js
@@ -1,10 +1,9 @@
 const jwt = require('jsonwebtoken')
 const config = require('../config/config')
 
-let tokenAuth = (findUserByToken) => (req, res, next) => {
+let tokenAuth = (findUserByToken) => async (req, res, next) => {
     let header = req.headers.authorization
     let token = header && header.split(' ')[1]
-
     if (token) {
         let payload
         try {
@@ -12,10 +11,9 @@ let tokenAuth = (findUserByToken) => (req, res, next) => {
         } catch (err) {
             return res.sendStatus(401)
         }
-        console.log(payload)
-        const user = findUserByToken(payload)
-        if (user) {
-            req.profile = user
+        const profile = await findUserByToken(payload)
+        if (profile) {
+            req.profile = profile
         } else {
             return res.sendStatus(401)
         }
