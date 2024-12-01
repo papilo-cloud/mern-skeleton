c1345f70ecddac0f3b7d7db99f206857ebd9fe9e
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-01 23:15:35 +0100

    Add routing component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,31 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'
+import { useContext } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Layout } from './Layout/Layout'
+import Home from './components/core/home/Home'
+import Users from './components/user/users/Users'
+import SignOut from './components/user/signout/SignOut'
+import { Context } from './context/Context'
+import Signin from './components/user/signin/Signin'
+import Signup from './components/user/signup/Signup'
+import Profile from './components/user/Profile'
 
 function App() {
-  const [count, setCount] = useState([])
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      const post = await axios.get('/api/users')
-      console.log(post.data)
-      setCount(post.data)
-    }
-    fetchPost()
-  }, [])
-  
+  const {login} = useContext(Context)
 
   return (
-    <>
-      {count.map(c => <li>{c.name } </li> )}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Layout /> }>
+          <Route index element={ <Home /> } />
+          <Route path='/users' element={ <Users /> } />
+          <Route path='/signout' element={ <SignOut />} />
+          <Route path='/signin' element={<Signin /> } />
+          <Route path='/signup' element={ <Signup /> } />
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
