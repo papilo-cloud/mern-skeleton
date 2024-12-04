8148a341cfbc46b1ecd57c297f2e8113311b49a2
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-04 21:49:10 +0100

    Fix Dialog component

--- client/src/components/core/DialogBox/Root.tsx
+++ client/src/components/core/DialogBox/Root.tsx
@@ -4,15 +4,27 @@ import clsx from 'clsx'
 
 interface RootProps extends ComponentProps<'div'> {
   children: ReactNode
-  open: boolean
+  close?: boolean
+  closeButton?: boolean
 }
 
-const Root = ({open, children, className, ...props}: RootProps) => {
+const Root = ({close, closeButton, children, className, ...props}: RootProps) => {
+  const handleClick = () => {
+    alert('clicked')
+  }
   return (
     <div className='dialog'>
         <div 
-          className={clsx('relative flex flex-col gap-2.5 bg-white rounded-md p-5 w-[300px] z-10 drop-shadow-lg ', className)}
+          className={clsx('pointer relative flex flex-col gap-3 bg-white rounded-md p-5 w-[300px] z-10 drop-shadow-lg ', className)}
            {...props} >
+            {
+              closeButton && (
+                <button 
+                  onClick={handleClick}
+                  className='absolute top-1 right-3 text-[#ff3c3c]'>X
+                </button>
+              )
+            }
             <div className='w-full'>{children?.[0 as keyof ReactNode]}</div>
             <div>{children?.[1 as keyof ReactNode]}</div>
             <div className='w-full'>
