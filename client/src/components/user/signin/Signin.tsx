981bc817a8afa3b9e4ef16d86571ef87d96c71ca
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-06 03:09:27 +0100

    Feat: use context provider for signin

--- client/src/components/user/signin/Signin.tsx
+++ client/src/components/user/signin/Signin.tsx
@@ -1,41 +1,26 @@
 import { Card, CardContent } from '../../core/Card/index'
 import { FormField, TextField, HelperText } from '../../core/Form/index'
 import {ButtonText, Button} from '../../core/Button/index.ts'
-import { signin } from '../../auth/api-auth'
 import { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { useAuth } from '../../../context/Context.tsx'
 
 
 const Signin = () => {
 
-  const [values, setValues] = useState({
-    email: '',
-    password: '',
-    error: '',
-    redirectToReferrer: false
-  })
-
+  const {auth} = useAuth()
+  console.log(useAuth())
   const handleChange = (name) => (event) => {
-    setValues({
-      ...values, 
+    auth.setValues({
+      ...auth.values,
       [name]: event.target.value
     })
-  } 
+  }
+
 
   const handleSubmit = (event) => {
     event.preventDefault()
-
-    const user = {
-      email: values.email || undefined,
-      password: values.password || undefined
-    }
-    signin(user).then(data => {
-      if (data.error) {
-        setValues({...values, error: data.error})
-      } else {
-        setValues({...values, error: '', redirectToReferrer: true})
-      }
-    })
+    auth.onSignin(auth.values)
   }
 
   const navigate = useNavigate()
@@ -48,13 +33,13 @@ const Signin = () => {
   console.log(from)
 
   useEffect(() => {
-    const {redirectToReferrer} = values
+    const {redirectToReferrer} = auth.values
     
     if (redirectToReferrer) {
       navigate(from)
     }
   
-  }, [values.redirectToReferrer])
+  }, [auth.values.redirectToReferrer])
  
   
 
@@ -67,15 +52,15 @@ const Signin = () => {
             className='py-2'
             placeholder='Email'
             type='email'
-            value={values.email}
+            value={auth.values.email}
             onChange={handleChange('email')}/>
           <TextField
             className='py-2'
             placeholder='Password'
             type='Password'
-            value={values.password}
+            value={auth.values.password}
             onChange={handleChange('password')} />
-          {values.error && <HelperText>{values.error}</HelperText>}
+          {auth.values.error && <HelperText>{auth.values.error}</HelperText>}
           <Button type='submit'>
             <ButtonText>Submit</ButtonText>
           </Button>
