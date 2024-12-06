414cf0c0f8ceab5288058b775be8bd31efb1b340
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-06 03:05:07 +0100

    Add private route component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,28 @@
-import { useContext } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Layout } from './Layout/Layout.tsx'
 import Home from './components/core/home/Home.tsx'
 import Users from './components/user/users/Users.tsx'
-import SignOut from './components/user/signout/SignOut.tsx'
-import { Context } from './context/Context.tsx'
 import Signin from './components/user/signin/Signin.tsx'
 import Signup from './components/user/signup/Signup.tsx'
 import Profile from './components/user/Profile.tsx'
+import PrivateRoute from './components/auth/PrivateRoute.tsx'
 
 function App() {
-
-  const login = useContext(Context)
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={ <Layout /> }>
-          <Route index element={ <Home /> } />
+          <Route index element={ <Home />} />
+          <Route path='/users/:userId' element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          } >
+          </Route>
           <Route path='/users' element={ <Users /> } />
-          <Route path='/signout' element={ <SignOut />} />
           <Route path='/signin' element={<Signin /> } />
           <Route path='/signup' element={ <Signup /> } />
-          <Route path='/profile' element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
