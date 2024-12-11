39989ef4a43a97c3e37b5c8665a2f81906d94c6a
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-11 22:15:05 +0100

    Modified

--- client/src/components/core/Form/index.ts
+++ client/src/components/core/Form/index.ts
@@ -2,6 +2,7 @@ import FormField from "./FormField";
 import HelperText from "./HelperText";
 import TextArea from "./TextArea";
 import TextField from "./TextField";
+import TextLabel from "./TextLabel";
 
 
-export {FormField, HelperText, TextArea, TextField}
+export {FormField, HelperText, TextArea, TextField, TextLabel}
