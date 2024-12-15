a18c8774cfb28f0d187e03f49fcea8d3b40f755c
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-12-16 00:43:52 +0100

    Rename folder

---
+++ client/src/components/Templates/api-user.js
@@ -0,0 +1,60 @@
+import axios from 'axios'
+
+const list = async () => {
+    try {
+        let response = await axios.get('/api/users')
+        return response
+    } catch (error) {
+        return error.response
+    }
+}
+
+const read = async (params, credentials, signal) => {
+    try {
+        const response = await axios.get(`/api/users/${params}`, {
+            signal,
+            headers: {
+                Authorization: `Bearer ${credentials}`
+            }
+        })
+        return  response
+    } catch (err) {
+        return err.response
+    }
+}
+
+const update = async (params, credentials, user) => {
+    try {
+        const response = await axios.put(`/api/users/${params}`, user, {
+            headers: {
+                Authorization: `Bearer ${credentials}`
+            }
+        })
+        return response.data
+    } catch (err) {
+        return err.response.data
+    }
+}
+
+const remove = async (params, credentials) => {
+    try {
+        const response = await axios.delete(`/api/users/${params}`, {
+            headers: {
+                Authorization: `Bearer ${credentials}`
+            }
+        })
+        return response
+    } catch (err) {
+        return err.response.data
+    }
+}
+
+const upload = async (data) => {
+    try {
+        const response = await axios.post('/api/upload', data)
+        return response
+    } catch (err) {
+        return err.response
+    }
+}
+export { list, read, update, remove, upload }
